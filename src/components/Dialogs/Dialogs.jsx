b09b50a9866622392dbb5f7e2f8891e6fdb552b9
Isckatel <nt-polza@yandex.ru>
2021-08-13 10:48:33 +0500

    Меню диалогов

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,9 +1,29 @@
 import css from "./Dialogs.module.css";
+import {NavLink} from "react-router-dom";
 
 const Dialogs = (props) => {
   return(
-    <div>
-      Dialogs
+    <div className={css.dialogs}>
+      <div className={css.dialogsItems}>
+        <NavLink to="" className={`${css.dialog} ${css.active}`}>
+          Dimych
+        </NavLink>
+        <NavLink to="" className={css.dialog}>
+          Ivan
+        </NavLink>
+        <NavLink to="" className={css.dialog}>
+          Slava
+        </NavLink>
+        <NavLink to="" className={css.dialog}>
+          Vika
+        </NavLink>
+      </div>
+      <div className={css.messages}>
+        <div className={css.message}>Привет!</div>
+        <div className={css.message}>Как тебе этот сайт?</div>
+        <div className={css.message}>Хорошо</div>
+      </div>
+
     </div>
   );
 }
