8e7d48e3a61033dbde619bff7a648a51c1c06bd3
Isckatel <nt-polza@yandex.ru>
2021-08-16 13:05:46 +0500

    Аватарки в диалогах.

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -4,32 +4,23 @@ import {NavLink} from "react-router-dom";
 const DialogItem = (props)=> {
   return (
     <NavLink to={"/dialogs/"+props.id} activeClassName={css.active} className={css.dialog}>
+      <img className={css.imgAva} src={props.urlAva} />
       {props.name}
     </NavLink>
   );
 }
 
 const Message = (props)=> {
-  return <div className={css.message}>{props.message}</div>
+  let mesRight = props.who == 'i'?css.mesRight:"";
+  return <div className={`${css.message} ${mesRight}`}>{props.message}</div>
 }
 
 const Dialogs = (props) => {
-  let dialogsData = [
-    {id: 1, name: 'Dimych'},
-    {id: 2, name: 'Ivan'},
-    {id: 3, name: 'Slava'},
-    {id: 4, name: 'Vika'}
-  ];
 
-  let messageData = [
-    {id:1, massage: 'Привет!'},
-    {id:2, massage: 'Как тебе этот сайт?'},
-    {id:3, massage: 'Хорошо.'}
-  ]
   //let gialogsElements = dialogsData.map( d => <DialogItem name={d.name} id={d.id}/> );
   //let gialogsElements = dialogsData.map( (d) => { <DialogItem name={d.name} id={d.id}/> });
-  let gialogsElements = props.messagesPage.dialogsData.map( dialog => <DialogItem name={dialog.name} id={dialog.id}/> );
-  let messageElements = props.messagesPage.messageData.map( message => <Message message={message.massage}/>);
+  let gialogsElements = props.messagesPage.dialogsData.map( dialog => <DialogItem name={dialog.name} id={dialog.id} urlAva={dialog.urlAva}/> );
+  let messageElements = props.messagesPage.messageData.map( message => <Message message={message.massage} who={message.who}/>);
 
   return(
     <div className={css.dialogs}>
--- src/redux/state.js
+++ src/redux/state.js
@@ -8,15 +8,15 @@ let state = {
   },
   messagesPage:{
     dialogsData: [
-      {id: 1, name: 'Dimych'},
-      {id: 2, name: 'Ivan'},
-      {id: 3, name: 'Slava'},
-      {id: 4, name: 'Vika'}
+      {id: 1, name: 'Dimych', urlAva: "https://thumbs.dreamstime.com/b/американский-оре-74019083.jpg"},
+      {id: 2, name: 'Ivan',   urlAva: "https://klike.net/uploads/posts/2019-03/1551511829_46.jpg"},
+      {id: 3, name: 'Slava',  urlAva: "https://shutniki.club/wp-content/uploads/2019/12/Avatarki_devushka_6_07164255.jpg"},
+      {id: 4, name: 'Vika',   urlAva: "https://st3.depositphotos.com/6179956/14764/i/950/depositphotos_147640065-stock-photo-portrait-of-sunny-girl-relaxing.jpg"}
     ],
     messageData: [
-      {id:1, massage: 'Привет!'},
-      {id:2, massage: 'Как тебе этот сайт?'},
-      {id:3, massage: 'Хорошо.'}
+      {id:1, massage: 'Привет!', who: 'i' },
+      {id:2, massage: 'Как тебе этот сайт?', who: 'i' },
+      {id:3, massage: 'Хорошо.', who: 'you' }
     ]
   }
 }
