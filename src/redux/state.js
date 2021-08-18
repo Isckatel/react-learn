f72726d9c2add19acc4fe10cc1b4a6e2273eb07a
Isckatel <nt-polza@yandex.ru>
2021-08-18 12:31:14 +0500

    Передача функции рендера в стейт через замыкание.

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,4 +1,4 @@
-import {rerenderEntireTree} from "./../render.js";
+ let rerenderEntireTree = () => { };
 
 let state = {
   profilePage:{
@@ -28,6 +28,10 @@ let state = {
     {id: 2, name: 'Slava',  urlAva: "https://shutniki.club/wp-content/uploads/2019/12/Avatarki_devushka_6_07164255.jpg"},
     {id: 3, name: 'Vika',   urlAva: "https://st3.depositphotos.com/6179956/14764/i/950/depositphotos_147640065-stock-photo-portrait-of-sunny-girl-relaxing.jpg"}
   ]
+}
+
+export const subscribe = (observer) => {
+  rerenderEntireTree = observer;
 }
 
 export let addPost = () => {
